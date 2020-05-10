220f53ad2e741330831d9d062b8e7ce9e101329b
FeelTheLemon <feelthelemon@desktop.lan>
2020-05-10 18:40:42 +0400

    added wifi auto reconnect behaviour

--- lib/WiFi/wifi.cpp
+++ lib/WiFi/wifi.cpp
@@ -9,6 +9,7 @@
 //needed for library
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
+#include <ESP8266WiFi.h>
 #include "WiFiManager.h"          //https://github.com/tzapu/WiFiManager
 
 
@@ -28,6 +29,9 @@ void WiFiE::init()
 	WiFiManager wifiManager;
 	//reset settings - for testing
 	//wifiManager.resetSettings();
+
+	WiFi.setAutoConnect(true);
+	WiFi.setAutoReconnect(true);
 
 	wifiManager.setAPCallback(configModeCallback);
 
